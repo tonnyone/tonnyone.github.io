6f2a7c20c22ab2440f1798417e702c9a6e1ea32b
liushaoqing <liushaoqing1@qq.com>
2021-02-19 16:40:09 +0800

    修改名称

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ const themeConfig = require('./config/themeConfig.js');
 module.exports = {
   theme: 'vdoing', // 使用依赖包主题
   theme: require.resolve('../../theme-vdoing'), // 使用本地主题
-  title: "刘少卿的Blog",
+  title: "",
   description: '布施、持戒、忍辱、精进、禅定、般若',
   // base: '/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
